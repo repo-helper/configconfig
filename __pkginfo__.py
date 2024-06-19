5cd496b5b5a9192433a32d6cc4ee8bfaf91ec5a2
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2024-06-19 10:29:24 +0100

    Depend on standard-imghdr for Python 3.13

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -3,7 +3,18 @@
 __all__ = ["extras_require"]
 
 extras_require = {
-		"sphinx": ["docutils", "sphinx<3.4.0,>=3.0.3", "sphinx-toolbox"],
+		"sphinx": [
+				"docutils",
+				"sphinx<3.4.0,>=3.0.3",
+				"sphinx-toolbox",
+				'standard-imghdr==3.10.14; python_version >= "3.13"'
+				],
 		"testing": ["pytest"],
-		"all": ["docutils", "pytest", "sphinx<3.4.0,>=3.0.3", "sphinx-toolbox"]
+		"all": [
+				"docutils",
+				"pytest",
+				"sphinx<3.4.0,>=3.0.3",
+				"sphinx-toolbox",
+				'standard-imghdr==3.10.14; python_version >= "3.13"'
+				]
 		}
